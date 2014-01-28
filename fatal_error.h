e1a0f5f76445e18071d78e53ab6669a8d160513d
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2014-01-28 15:03:55 +0100

    add fatal_error function to cleanly exit the program with an error code and a custom error message printed by g_cart_id==0 only

---
+++ fatal_error.h
@@ -0,0 +1,27 @@
+/***********************************************************************
+ *
+ * Copyright (C)       2013 Bartosz Kostrzewa
+ *
+ * This file is part of CVC.
+ *
+ * CVC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CVC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CVC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+ 
+ #ifndef _fatal_error_H
+ #define _fatal_error_H
+ 
+ void fatal_error(const unsigned int signal, const char * format, ...);
+ 
+ #endif /* _fatal_error_H */
